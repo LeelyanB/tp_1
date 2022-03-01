5ac770df4c47c03e5018024ddd8e300744dfc029
LeelyanB <100620473+LeelyanB@users.noreply.github.com>
2022-03-01 21:43:37 +0100

    Create Syracuse_ameliorer.py

---
+++ Syracuse_ameliorer.py
@@ -0,0 +1,32 @@
+lass nombreSyracuse:
+
+  def __init__(self, nom):
+      self.nom = nom
+      self.contenue = 0
+  
+  def recuperation(self):
+     fichier = open(self.nom,"r")
+     self.contenue = fichier.read()
+     self.contenue = int(self.contenue)
+     print(self.contenue)
+     fichier.close()
+     
+
+  def operation(self): 
+    
+        l = []
+        while self.contenue!=1:
+          if (self.contenue%2)==0:
+              self.contenue= self.contenue/2
+          else:
+            self.contenue=self.contenue*3+1
+          l.append(self.contenue,)
+        fichier =open(self.nom,"w")  
+        for i in l :
+         i = str(i) + " "    
+         fichier.write(i)
+        fichier.close()
+
+nbr = nombreSyracuse(input("fichier"))
+nbr.recuperation()
+nbr.operation()
